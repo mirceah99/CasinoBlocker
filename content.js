cb6b12ee2438c9fc98301497788bfc1d0d9421b5
Alexandru Pop <alexandrupop@ucs.ro>
2025-03-29 16:25:03 +0200

    Better Logo

--- content.js
+++ content.js
@@ -1,10 +1,21 @@
-const bannedSites = [
-  "www.princesscasino.ro",
-  "bannedwebsite.com",
-  "anotherbadsite.com",
-];
+fetch(chrome.runtime.getURL('bannedSites.json'))
+		.then(response => response.json())
+		.then(bannedSites => {
+			if (bannedSites.some(site => window.location.hostname.includes(site))) {
+				const doc = document.documentElement;
+				const oldHead = document.head;
+				const oldBody = document.body;
 
-if (bannedSites.some((site) => window.location.hostname.includes(site))) {
-  document.documentElement.innerHTML = "Lasa bets";
-  window.stop();
-}
+				doc.removeChild(oldHead)
+				doc.removeChild(oldBody);
+
+				const head = document.createElement('head');
+				const body = document.createElement('body');
+
+				body.insertAdjacentHTML('beforeend', '<h1>Hello</h1>');
+
+				doc.appendChild(head);
+				doc.appendChild(body);
+			}
+		})
+		.catch(error => console.error('Error loading bannedSites.json:', error));
